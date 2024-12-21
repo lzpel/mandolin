1642df0eab43d44a3b1dadea389f56f9d355dee6
lzpel <18492524+lzpel@users.noreply.github.com>
2024-12-22 05:41:02 +0900

    udpate

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ generate openapi-base server code
 ## my favorite mandolin music
 
 - 月に舞う/武藤理恵 https://youtu.be/OVKkRj0di2I?si=bZn0yI6C9QrVjPKV
+- Suite Spagnola https://youtu.be/fCkcP_cuneU?si=21Da2RJgVRyPD8Et
--- examples/example_axum.rs
+++ examples/example_axum.rs
@@ -1,5 +1,14 @@
+use std::fs;
 use mandolin;
 // cargo run --example example_axum
 fn main(){
+	//エラーをまとめる方法を考える
+	mandolin::Mandolin::new_from_path("./test_openapi/openapi_petstore.yaml")
+		.unwrap()
+		.unwrap()
+		.template(fs::File::open("./test_openapi/openapi_petstore.yaml").unwrap())
+		.unwrap()
+		.template(fs::File::open("./test_openapi/openapi_petstore.yaml").unwrap())
+		.unwrap();
 	mandolin::build("./test_openapi/openapi_petstore.yaml", "./output/openapi_petstore.rs").expect("TODO: panic message");
 }
