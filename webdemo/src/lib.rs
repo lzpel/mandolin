a5fbdea2780b0f8f1189d6b314d0a7fe80826935
lzpel <18492524+lzpel@users.noreply.github.com>
2024-12-25 17:54:27 +0900

    confirm: example callable

--- webdemo/src/lib.rs
+++ webdemo/src/lib.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use std::io::Cursor;
 use mandolin;
 use wasm_bindgen::prelude::*;
@@ -7,12 +6,18 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 #[wasm_bindgen]
-pub fn example(openapi: &str) {
+pub fn example(openapi: &str) -> String {
 	//エラーをまとめる方法を考える
+	let mut buffer = Cursor::new(Vec::new());
+	let reader = Cursor::new(include_bytes!("../../builtin/main.tera"));
 	mandolin::Mandolin::new(Cursor::new(openapi.as_bytes()))
 		.unwrap()
-		.template_from_path("builtin/main.tera")
+		.template(reader)
+		.unwrap()
+		.write(&mut buffer)
+		.unwrap()
 		.unwrap();
+	String::from_utf8(buffer.into_inner()).expect("Found invalid UTF-8")
 }
 
 #[cfg(test)]
@@ -35,7 +40,11 @@ mod tests {
 			.write(&mut buffer)
 			.unwrap()
 			.unwrap();
-        let result = String::from_utf8(buffer.into_inner()).expect("Found invalid UTF-8");
-        println!("{}", result);
+		let result = String::from_utf8(buffer.into_inner()).expect("Found invalid UTF-8");
+		println!("{}", result);
+	}
+	#[test]
+	fn generate2() {
+		println!("{}", example(include_str!("../../test_openapi/openapi_petstore.yaml")));
 	}
 }
