a8fa6edc37592c09e3772fa37f2e4aef8e20ffca
lzpel <18492524+lzpel@users.noreply.github.com>
2025-01-05 18:05:01 +0900

    add: right arrow

--- demo/app/compare.tsx
+++ demo/app/compare.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import {example} from "@/lib";
 export default function Compare(props: {openapi: string}) {
 	const [text, setText] = React.useState(props.openapi)
-	const textareaStyle: React.CSSProperties = { flex: 1, width: '50%', height: '100%', boxSizing: 'border-box', border: '1px solid #ccc', padding: '10px', resize: "none"};
-	return (<div style={{flexGrow: 1, display: "flex"}}>
+	const textareaStyle: React.CSSProperties = { flexGrow: 1, height: '100%', boxSizing: 'border-box', border: '1px solid #ccc', padding: '10px', resize: "none"};
+	return (<div style={{flexGrow: 1, display: "flex", alignItems: "center"}}>
 				<textarea style={textareaStyle} value={text} onChange={v=>setText(v.currentTarget.value)}/>
+				<div style={{fontSize: "xxx-large"}}>→</div>
 				<textarea style={textareaStyle} value={example(text)} readOnly={true}/>
 			</div>
 	)
