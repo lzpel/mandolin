58a150dd360a876735d2bc56d266b911c2171146
lzpel <18492524+lzpel@users.noreply.github.com>
2025-01-12 04:47:04 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -91,79 +91,95 @@ impl Mandolin {
 		self
 	}
 	pub fn render(&self) -> Result<String, tera::Error> {
-		let mut tera = Tera::default();
 		let json = serde_json::to_value(&self.api)?;
-		// 空の辞書を返す関数
-		tera.register_function("m", |_: &HashMap<String, tera::Value>| {
-			Ok(tera::Value::Object(Default::default()))
-		});
-		// JsonPointer文字列を実態に変換するフィルター
-		let json_for_p=json.clone();
-		tera.register_filter("p", move |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-			let default = Ok(tera::Value::Object(Default::default()));
-			let path = value.as_str().unwrap_or_default();
-			let mut parent = &json_for_p;
-			for p in path.split("/").skip(1) {
-				let p=p.replace("~0","~").replace("~1","/");// RFC6901
-				parent = if let serde_json::Value::Object(map) = parent {
-					match map.get(p.as_str()) {
-						None => return default,
-						Some(latest) => latest,
-					}
-				} else if let serde_json::Value::Array(array) = parent {
-					match p.parse::<usize>() {
-						Ok(v) => match array.get(v) {
+		let r={
+			let mut tera = Tera::default();
+			// 空の辞書を返す関数
+			tera.register_function("m", |_: &HashMap<String, tera::Value>| {
+				Ok(tera::Value::Object(Default::default()))
+			});
+			// JsonPointer文字列を実態に変換するフィルター
+			let p=|path: &str, no_err| {
+				let default = if no_err {Ok(tera::Value::Object(Default::default()))} else {Err(tera::Error::from(format!("p: {} not found", path)))};
+				let mut parent = json.clone();
+				for p in path.split("/").skip(1) {
+					let p=p.replace("~0","~").replace("~1","/");// RFC6901
+					parent = if let serde_json::Value::Object(mut map) = parent {
+						match map.remove(p.as_str()) {
 							None => return default,
 							Some(latest) => latest,
-						},
-						Err(_) => return default,
+						}
+					} else if let serde_json::Value::Array(mut array) = parent {
+						match p.parse::<usize>() {
+							Ok(v) => match v<array.len() {//removeでは他の要素の前詰めで遅いのでswap_removeで高速化
+								false => return default,
+								true => array.swap_remove(v),
+							},
+							Err(_) => return default,
+						}
+					} else {
+						return default;
 					}
-				} else {
-					return default;
 				}
-			}
-			return Ok(parent.clone());
-		});
-		tera.register_filter("ref", |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-			let i = tera::try_get_value!("ref", "value", ReferenceOr<()>, value);
-			let v = match i {
-				ReferenceOr::Reference { reference } => reference.replace("#/components/schemas/", "").to_string(),
-				ReferenceOr::Item(_) => "".to_string(),
+				return Ok(parent);
 			};
-			Ok(tera::to_value(v).unwrap())
-		});
-		tera.register_filter("content_into_media", |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-			let i = tera::try_get_value!("content_into_media", "value", Content, value);
-			LappedMediaType::try_from(&i)
-				.map(|v| tera::to_value(v).unwrap())
-				.map_err(|_| tera::Error::from("content_into_media: no content"))
-		});
-		tera.register_filter("paths_into_operations", |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-			let paths = tera::try_get_value!("paths_into_operations", "value", Paths, value);
-			let operations: Vec<LappedOperation> = paths.iter()
-				.filter(|(_, path)| path.as_item().is_some())
-				.map(|(path_key, path)| {
-					let path = path.as_item().unwrap();
-					[
-						("get", path.get.clone()),
-						("delete", path.delete.clone()),
-						("head", path.head.clone()),
-						("options", path.options.clone()),
-						("patch", path.patch.clone()),
-						("post", path.post.clone()),
-						("put", path.put.clone()),
-						("trace", path.trace.clone()),
-					]
-						.into_iter()
-						.filter(|(_method, operation)| operation.is_some())
-						.map(|(method, operation)| LappedOperation::new(path_key, method, &operation.unwrap()))
-				})
-				.flatten()
-				.collect();
-			tera::to_value(operations).map_err(|e| tera::Error::from(e.to_string()))
-		});
-		let context = Context::from_serialize(&self.api)?;
-		tera.render_str(self.templates.join("\n").as_str(), &context)
+			let r=|value: &tera::Value, no_err: bool|{
+				match tera::try_get_value!("r", "value", ReferenceOr<()>, value) {
+					ReferenceOr::Reference { reference } => p(reference.as_str(), no_err),
+					ReferenceOr::Item(_) => Ok(value.clone()),
+				}
+			};
+			tera.register_filter("p", |value: &tera::Value, _: &HashMap<String, tera::Value>|{
+				p(value.as_str().unwrap_or_default(), true)
+			});
+			tera.register_filter("r", |value: &tera::Value, _: &HashMap<String, tera::Value>|{
+				r(value, false)
+			});
+			tera.register_filter("pr", |value: &tera::Value, _: &HashMap<String, tera::Value>|{
+				r(&p(value.as_str().unwrap_or_default(), true).unwrap(), false)
+			});
+			tera.register_filter("ref", |value: &tera::Value, _: &HashMap<String, tera::Value>| {
+				let i = tera::try_get_value!("ref", "value", ReferenceOr<()>, value);
+				let v = match i {
+					ReferenceOr::Reference { reference } => reference.replace("#/components/schemas/", "").to_string(),
+					ReferenceOr::Item(_) => "".to_string(),
+				};
+				Ok(tera::to_value(v).unwrap())
+			});
+			tera.register_filter("content_into_media", |value: &tera::Value, _: &HashMap<String, tera::Value>| {
+				let i = tera::try_get_value!("content_into_media", "value", Content, value);
+				LappedMediaType::try_from(&i)
+					.map(|v| tera::to_value(v).unwrap())
+					.map_err(|_| tera::Error::from("content_into_media: no content"))
+			});
+			tera.register_filter("paths_into_operations", |value: &tera::Value, _: &HashMap<String, tera::Value>| {
+				let paths = tera::try_get_value!("paths_into_operations", "value", Paths, value);
+				let operations: Vec<LappedOperation> = paths.iter()
+					.filter(|(_, path)| path.as_item().is_some())
+					.map(|(path_key, path)| {
+						let path = path.as_item().unwrap();
+						[
+							("get", path.get.clone()),
+							("delete", path.delete.clone()),
+							("head", path.head.clone()),
+							("options", path.options.clone()),
+							("patch", path.patch.clone()),
+							("post", path.post.clone()),
+							("put", path.put.clone()),
+							("trace", path.trace.clone()),
+						]
+							.into_iter()
+							.filter(|(_method, operation)| operation.is_some())
+							.map(|(method, operation)| LappedOperation::new(path_key, method, &operation.unwrap()))
+					})
+					.flatten()
+					.collect();
+				tera::to_value(operations).map_err(|e| tera::Error::from(e.to_string()))
+			});
+			let context = Context::from_serialize(&self.api)?;
+			tera.render_str(self.templates.join("\n").as_str(), &context).unwrap()
+		};
+		Ok(r)
 	}
 }
 #[cfg(test)]
