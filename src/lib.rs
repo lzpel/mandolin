83691a88bed78ba448c18e6f75d1ee65a21b3e24
lzpel <18492524+lzpel@users.noreply.github.com>
2025-01-14 19:33:25 +0900

    confirm: test_filters with minijinja

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 pub mod templates;
 mod utils;
 
-use openapiv3::{Content, MediaType, OpenAPI, Operation, Paths, ReferenceOr, RequestBody};
+use openapiv3::{Content, MediaType, OpenAPI, Operation, ReferenceOr, RequestBody};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use std::collections::{BTreeMap, HashMap};
-use std::ops::Index;
 use std::sync::LazyLock;
-use tera::{Context, Error, Tera};
 
 #[derive(Serialize, Deserialize)]
 pub struct LappedMediaType {
@@ -87,8 +84,12 @@ impl LappedOperation {
         }
     }
 }
-static EMPTY_OBJECT: LazyLock<Value> = LazyLock::new(|| Value::Object(Default::default()));
-static EMPTY_ARRAY: LazyLock<Value> = LazyLock::new(|| Value::Array(Default::default()));
+#[derive(Serialize, Deserialize)]
+pub struct Empty {}
+static EMPTY_OBJECT: LazyLock<minijinja::Value> =
+    LazyLock::new(|| minijinja::Value::from_serialize(Empty {}));
+static EMPTY_ARRAY: LazyLock<minijinja::Value> =
+    LazyLock::new(|| minijinja::Value::from_serialize::<[(); 0]>([(); 0]));
 
 pub struct Mandolin {
     api: OpenAPI,
@@ -105,28 +106,33 @@ impl Mandolin {
         self.templates.push(template.as_ref().to_string());
         self
     }
-    fn p<'a>(api: &'a Value, path: &str, no_err: bool) -> Result<&'a Value, Error> {
+    fn p(
+        api: minijinja::Value,
+        path: &str,
+        no_err: bool,
+    ) -> Result<minijinja::Value, minijinja::Error> {
         let default = if no_err {
-            Ok(&*EMPTY_OBJECT)
+            Ok(EMPTY_OBJECT.clone())
         } else {
-            Err(tera::Error::from(format!("p: {path} not found")))
+            Err(minijinja::Error::new(
+                minijinja::ErrorKind::NonKey,
+                format!("p: {path} not found"),
+            ))
         };
         let mut parent = api;
         for p in path.split("/").skip(1) {
             let p = p.replace("~0", "~").replace("~1", "/"); // RFC6901
-            parent = if let serde_json::Value::Object(map) = parent {
-                match map.get(p.as_str()) {
-                    None => return default,
-                    Some(latest) => latest,
-                }
-            } else if let serde_json::Value::Array(array) = parent {
-                match p.parse::<usize>() {
-                    Ok(v) => match v < array.len() {
-                        //removeでは他の要素の前詰めで遅いのでswap_removeで高速化
-                        false => return default,
-                        true => array.index(v),
+            parent = if let Some(map) = parent.as_object() {
+                match map.get_value(&minijinja::Value::from(&p)) {
+                    None => match p
+                        .parse::<usize>()
+                        .ok()
+                        .and_then(|i| map.get_value(&minijinja::Value::from(i)))
+                    {
+                        None => return default,
+                        Some(latest) => latest,
                     },
-                    Err(_) => return default,
+                    Some(latest) => latest,
                 }
             } else {
                 return default;
@@ -134,175 +140,146 @@ impl Mandolin {
         }
         Ok(parent)
     }
-    fn r<'a>(api: &'a Value, value: &'a tera::Value, no_err: bool) -> Result<&'a Value, Error> {
-        match tera::try_get_value!("r", "value", ReferenceOr<tera::Value>, value) {
-            ReferenceOr::Reference { reference } => Self::p(api, reference.as_str(), no_err),
-            ReferenceOr::Item(_) => Ok(value),
+    fn r<'a>(
+        api: minijinja::Value,
+        value: minijinja::Value,
+        no_err: bool,
+    ) -> Result<minijinja::Value, minijinja::Error> {
+        match value.downcast_object_ref::<ReferenceOr<minijinja::Value>>() {
+            Some(ReferenceOr::Reference { reference }) => Self::p(api, reference.as_str(), no_err),
+            _ => Ok(value),
         }
     }
-    fn pr<'a>(api: &'a Value, path: &str, no_err: bool) -> Result<&'a Value, Error> {
-        let v = Self::p(&api, path, no_err)?;
-        Self::r(&api, v, false)
-    }
-    fn ls<'a>(
-        api: &'a Value,
+    fn pr<'a>(
+        api: minijinja::Value,
         path: &str,
         no_err: bool,
-    ) -> Result<BTreeMap<String, &'a Value>, Error> {
-        let v = match Self::pr(api, path, no_err)? {
-            serde_json::Value::Object(map) => map
-                .into_iter()
-                .map(|(k, v)| (format!("{path}/{k}"), v))
-                .collect(),
-            serde_json::Value::Array(vec) => vec
-                .into_iter()
-                .enumerate()
-                .map(|(k, v)| (format!("{path}/{k}"), v))
-                .collect(),
-            _ => {
-                return if no_err {
-                    [].into_iter().collect()
-                } else {
-                    Err(tera::Error::from(format!("ls {}", path)))
-                }
-            }
-        };
-        Ok(v)
+    ) -> Result<minijinja::Value, minijinja::Error> {
+        let v = Self::p(api.clone(), path, no_err)?;
+        Self::r(api, v, false)
     }
-    fn lsop<'a>(api: &'a Value, path: &str, no_err: bool) -> Result<BTreeMap<String, &'a Value>, Error> {
-        let v = Self::ls(api, path, no_err)?;
-		let methods=["get", "put", "post", "delete", "options", "head", "patch", "trace"];
+    fn ls(
+        api: minijinja::Value,
+        path: &str,
+        no_err: bool,
+    ) -> Result<BTreeMap<String, minijinja::Value>, minijinja::Error> {
+        let v = Self::pr(api, path, no_err)?;
+        let default = if no_err {
+            Ok(Default::default())
+        } else {
+            Err(minijinja::Error::new(
+                minijinja::ErrorKind::NonKey,
+                format!("ls {}", path),
+            ))
+        };
+        Ok(if let Some(v) = v.as_object() {
+            if let Some(v) = v.try_iter_pairs() {
+                v.map(|(k, v)| {
+                    (
+                        format!("{path}/{}", k.to_string()),
+                        v,
+                    )
+                })
+                .collect()
+            } else if let Some(v) = v.try_iter() {
+                v.enumerate()
+                    .map(|(k, v)| (format!("{path}/{}", k), v))
+                    .collect()
+            } else {
+                return default;
+            }
+        } else {
+            return default;
+        })
+    }
+    fn lsop(
+        api: minijinja::Value,
+        path: &str,
+        no_err: bool,
+    ) -> Result<BTreeMap<String, minijinja::Value>, minijinja::Error> {
+        let v = Self::ls(api.clone(), path, no_err)?;
+        let methods = [
+            "get", "put", "post", "delete", "options", "head", "patch", "trace",
+        ];
         let w = v
-            .into_iter()
+            .iter()
             .map(|(k, v_path)| {
-                Self::ls(api, k.as_str(), no_err).unwrap_or_default()
+                Self::ls(api.clone(), k.as_str(), no_err)
+                    .unwrap_or_default()
                     .into_iter()
                     .map(move |(k, v)| (k, v_path, v))
             })
             .flatten()
             .filter(|(k, _, _)| methods.iter().any(|v| k.ends_with(v)))
-            .map(|(k, v, w)| (k, w))// 本来はここで親元のparametersを合成することができるはず
+            .map(|(k, _, w)| (k, w)) // 本来はここで親元のparametersを合成することができるはず
             .collect();
         Ok(w)
     }
-    fn value_cloned<'a, M: IntoIterator<Item = (String, &'a Value)>>(m: M) -> Value {
-        Value::Object(m.into_iter().map(|(k, v)| (k, v.clone())).collect())
-    }
-    pub fn render(&self) -> Result<String, tera::Error> {
-        let api = serde_json::to_value(&self.api)?;
-        let mut tera = Tera::default();
-        // 空の辞書を返す関数
-        tera.register_function("m", |_: &HashMap<String, tera::Value>| {
-            Ok(tera::Value::Object(Default::default()))
-        });
+    pub fn render(&self) -> Result<String, minijinja::Error> {
+        let mut env = minijinja::Environment::new();
+        println!("{:?}", env);
+        let api = minijinja::Value::from_serialize(&self.api);
+        env.add_filter("json_encode", minijinja::filters::tojson);
+        env.add_function("m", || { Ok(minijinja::Value::from_serialize(Empty{})) });
         {
             let api = api.clone();
-            tera.register_filter(
+            env.add_filter(
                 "p",
-                move |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                    Self::p(&api, value.as_str().unwrap_or_default(), true).cloned()
+                move |value: &minijinja::Value| {
+                    Self::p(api.clone(), value.as_str().unwrap_or_default(), true)
                 },
             );
         }
         {
             let api = api.clone();
-            tera.register_filter(
+            env.add_filter(
                 "r",
-                move |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                    Self::r(&api, value, false).cloned()
+                move |value: minijinja::Value| {
+                    Self::r(api.clone(), value, false)
                 },
             );
         }
         {
             let api = api.clone();
-            tera.register_filter(
+            env.add_filter(
                 "pr",
-                move |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                    Self::pr(&api, value.as_str().unwrap_or_default(), true).cloned()
+                move |value: &minijinja::Value| {
+                    Self::pr(api.clone(), value.as_str().unwrap_or_default(), true)
                 },
             );
         }
         {
             let api = api.clone();
-            tera.register_filter(
+            env.add_filter(
                 "ls",
-                move |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                    Self::ls(&api, value.as_str().unwrap_or_default(), true).map(|v| Self::value_cloned(v))
+                move |value: &minijinja::Value| {
+                    Self::ls(api.clone(), value.as_str().unwrap_or_default(), true)
                 },
             );
         }
         {
             let api = api.clone();
-            tera.register_function(
-                "ls",
-                move |value: &HashMap<String, tera::Value>| {
-                    Self::ls(&api, value.values().next().unwrap().as_str().unwrap_or_default(), true).map(|v| Self::value_cloned(v))
-                },
-            );
+            env.add_function("ls", move |value: &HashMap<String, minijinja::Value>| {
+                Self::ls(
+                    api.clone(),
+                    value.values().next().unwrap().as_str().unwrap_or_default(),
+                    true,
+                )
+            });
         }
         {
             let api = api.clone();
-            tera.register_filter(
+            env.add_filter(
                 "lsop",
-                move |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                    Self::lsop(&api, value.as_str().unwrap_or_default(), true).map(|v| Self::value_cloned(v))
+                move |value: &minijinja::Value| {
+                    Self::lsop(api.clone(), value.as_str().unwrap_or_default(), true)
                 },
             );
         }
-        tera.register_filter(
-            "ref",
-            |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                let i = tera::try_get_value!("ref", "value", ReferenceOr<()>, value);
-                let v = match i {
-                    ReferenceOr::Reference { reference } => {
-                        reference.replace("#/components/schemas/", "").to_string()
-                    }
-                    ReferenceOr::Item(_) => "".to_string(),
-                };
-                Ok(tera::to_value(v).unwrap())
-            },
-        );
-        tera.register_filter(
-            "content_into_media",
-            |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                let i = tera::try_get_value!("content_into_media", "value", Content, value);
-                LappedMediaType::try_from(&i)
-                    .map(|v| tera::to_value(v).unwrap())
-                    .map_err(|_| tera::Error::from("content_into_media: no content"))
-            },
-        );
-        tera.register_filter(
-            "paths_into_operations",
-            |value: &tera::Value, _: &HashMap<String, tera::Value>| {
-                let paths = tera::try_get_value!("paths_into_operations", "value", Paths, value);
-                let operations: Vec<LappedOperation> = paths
-                    .iter()
-                    .filter(|(_, path)| path.as_item().is_some())
-                    .map(|(path_key, path)| {
-                        let path = path.as_item().unwrap();
-                        [
-                            ("get", path.get.clone()),
-                            ("delete", path.delete.clone()),
-                            ("head", path.head.clone()),
-                            ("options", path.options.clone()),
-                            ("patch", path.patch.clone()),
-                            ("post", path.post.clone()),
-                            ("put", path.put.clone()),
-                            ("trace", path.trace.clone()),
-                        ]
-                        .into_iter()
-                        .filter(|(_method, operation)| operation.is_some())
-                        .map(|(method, operation)| {
-                            LappedOperation::new(path_key, method, &operation.unwrap())
-                        })
-                    })
-                    .flatten()
-                    .collect();
-                tera::to_value(operations).map_err(|e| tera::Error::from(e.to_string()))
-            },
-        );
-        let context = Context::from_serialize(&self.api)?; //いずれ消したい
-        tera.render_str(self.templates.join("\n").as_str(), &context)
+        let v=self.templates.join("\n");
+        env.add_template("main", v.as_str())?;
+        let template = env.get_template("main")?;
+        template.render(&self.api)
     }
 }
 #[cfg(test)]
@@ -338,7 +315,7 @@ mod tests {
     fn test_filter() {
         let v = apis().get("openapi.yaml").unwrap().clone();
         let r = Mandolin::new(v)
-            .template("{{'#'|p|json_encode()}}\n{{'#/paths'|p|json_encode()}}\n{{'#/servers/0'|p|json_encode()}}\n{{'#'|ls|json_encode()}}{{'#/servers'|ls|json_encode()}}\n{{'#/paths'|lsop|json_encode()}}")
+            .template("{{'#'|p|json_encode}}\n{{'#/paths'|p|json_encode}}\n{{'#/servers/0'|p|json_encode}}\n{{'#'|ls|json_encode}}{{'#/servers'|ls|json_encode}}\n{{'#/paths'|lsop|json_encode}}")
             .render()
             .unwrap();
         println!("{}", r)
