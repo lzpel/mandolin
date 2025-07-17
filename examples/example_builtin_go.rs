b4f355dc5b0060fbf5773a4a4d2f5c2de9e9328a
lzpel <18492524+lzpel@users.noreply.github.com>
2025-07-18 00:10:55 +0900

    dev

--- examples/example_builtin_go.rs
+++ examples/example_builtin_go.rs
@@ -1,6 +1,7 @@
 use mandolin;
 use serde_yaml;
 use std::fs;
+// go template is under developping
 fn main() {
 	// read openapi.yaml
 	let input_string = fs::read_to_string("./openapi/openapi_petstore.yaml").unwrap();
@@ -8,6 +9,10 @@ fn main() {
 	// make environment
 	let env = mandolin::environment(input_api).unwrap();
 	// write the rendered output
-	let output = env.get_template("RUST_AXUM").unwrap().render(0).unwrap();
-	fs::write("./out/server_builtin_axum.rs", output).unwrap();
+	let output = env
+		.get_template("GO_SERVER")
+		.unwrap()
+		.render(0)
+		.unwrap();
+	fs::write("./out/server_builtin_go.go", output).unwrap();
 }
--- examples/example_builtin_hono.rs
+++ examples/example_builtin_hono.rs
@@ -3,11 +3,15 @@ use serde_yaml;
 use std::fs;
 fn main() {
 	// read openapi.yaml
-	let input_string=fs::read_to_string("./openapi/openapi_petstore.yaml").unwrap();
+	let input_string = fs::read_to_string("./openapi/openapi_petstore.yaml").unwrap();
 	let input_api = serde_yaml::from_str(&input_string.as_str()).unwrap();
 	// make environment
 	let env = mandolin::environment(input_api).unwrap();
 	// write the rendered output
-	let output = env.get_template("TYPESCRIPT_HONO").unwrap().render(0).unwrap();
+	let output = env
+		.get_template("TYPESCRIPT_HONO")
+		.unwrap()
+		.render(0)
+		.unwrap();
 	fs::write("./out/server_builtin_hono.ts", output).unwrap();
 }
