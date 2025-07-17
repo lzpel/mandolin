47bc187551e4471760ca935300c9926bf0a011f1
lzpel <18492524+lzpel@users.noreply.github.com>
2025-07-17 21:34:53 +0900

    update README.md

--- examples/example_builtin_axum.rs
+++ examples/example_builtin_axum.rs
@@ -3,14 +3,11 @@ use serde_yaml;
 use std::fs;
 fn main() {
 	// read openapi.yaml
-	let input_api = serde_yaml::from_str(
-		fs::read_to_string("./openapi/openapi_petstore.yaml")
-			.unwrap()
-			.as_str(),
-	)
-	.unwrap();
+	let input_string=fs::read_to_string("./openapi/openapi_petstore.yaml").unwrap();
+	let input_api = serde_yaml::from_str(&input_string.as_str()).unwrap();
+	// make environment
 	let env = mandolin::environment(input_api).unwrap();
 	// write the rendered output
 	let output = env.get_template("RUST_AXUM").unwrap().render(0).unwrap();
-	fs::write("./out/server_builtin.ts", output).unwrap();
+	fs::write("./out/server_builtin_axum.rs", output).unwrap();
 }
