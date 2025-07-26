b4f599ab46c31b0b34711e726ecadd9c2686b33d
lzpel <18492524+lzpel@users.noreply.github.com>
2025-07-27 03:21:49 +0900

    monaco-editor

--- frontend/app/compare.tsx
+++ frontend/app/compare.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from "react";
 import {example, templates} from "@/out";
+import Editor from '@monaco-editor/react';
 
 export default function Compare(props: { files: { [key: string]: string } }) {
 	const [text, setText] = React.useState(props.files["openapi_petstore.yaml"])
@@ -11,8 +12,6 @@ export default function Compare(props: { files: { [key: string]: string } }) {
 		height: '100%',
 		boxSizing: 'border-box',
 		border: '1px solid #ccc',
-		padding: '10px',
-		resize: "none"
 	};
 	const preset=Object.entries(props.files).map(([key, value]) => <button key={key} onClick={() => setText(value)}>{key}</button>)
 	const framework=templates().map(v=><label key={v}><input type="radio" name="template" value="RUST_AXUM" required checked={selectedTemplate===v} onChange={()=>setSelectedTemplate(v)}/>{v}</label>)
@@ -20,9 +19,20 @@ export default function Compare(props: { files: { [key: string]: string } }) {
 		<span>preset openapi: {preset}</span>
 		<span>target framework:{framework}</span>
 		<div style={{flexGrow: 1, display: "flex", alignItems: "center"}}>
-			<textarea style={textareaStyle} value={text} onChange={v => setText(v.currentTarget.value)}/>
+			<div style={textareaStyle}>
+				<Editor
+					defaultLanguage={"yaml"}
+					value={text}
+					onChange={v => setText(v||"")}
+				/>
+			</div>
 			<div style={{fontSize: "xxx-large"}}>→</div>
-			<textarea style={textareaStyle} value={example(text, selectedTemplate)} readOnly={true}/>
+			<div style={textareaStyle}>
+				<Editor
+					defaultLanguage={selectedTemplate.includes("RUST") ? "rust": "typescript"}
+					value={example(text, selectedTemplate)}
+				/>
+			</div>
 		</div>
 	</>
 }
